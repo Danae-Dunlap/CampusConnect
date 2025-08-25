ddc1c79f836bd056032aab1c8df3d556dec48a02
Danae-Dunlap <109568020+Danae-Dunlap@users.noreply.github.com>
2025-08-24 21:39:17 -0400

    Convert server files from js -> ts

--- campusconnect/src/server/records.ts
+++ campusconnect/src/server/records.ts
@@ -4,7 +4,7 @@ import express from "express";
 import db from "./connection.js";
 
 // This help convert the id from string to ObjectId for the _id.
-import { ObjectId } from "mongodb";
+import { ObjectId, type DeleteResult, type Document, type InsertOneResult, type UpdateResult, type WithId } from "mongodb";
 
 // router is an instance of the express router.
 // We use it to define our routes.
@@ -12,14 +12,15 @@ import { ObjectId } from "mongodb";
 const router = express.Router();
 
 // This section will help you get a list of all the records.
-router.get("/", async (req, res) => {
+/*
+router.get("/", async (_req: any, res: { send: (arg0: WithId<Document>[]) => { (): any; new(): any; status: { (arg0: number): void; new(): any; }; }; }) => {
   let collection = await db.collection("records");
   let results = await collection.find({}).toArray();
   res.send(results).status(200);
 });
 
 // This section will help you get a single record by id
-router.get("/:id", async (req, res) => {
+router.get("/:id", async (req: { params: { id: number; }; }, res: { send: (arg0: string | WithId<Document>) => { (): any; new(): any; status: { (arg0: number): void; new(): any; }; }; }) => {
   let collection = await db.collection("records");
   let query = { _id: new ObjectId(req.params.id) };
   let result = await collection.findOne(query);
@@ -29,7 +30,7 @@ router.get("/:id", async (req, res) => {
 });
 
 // This section will help you create a new record.
-router.post("/", async (req, res) => {
+router.post("/", async (req: { body: { name: any; position: any; level: any; }; }, res: { send: (arg0: InsertOneResult<Document>) => { (): any; new(): any; status: { (arg0: number): void; new(): any; }; }; status: (arg0: number) => { (): any; new(): any; send: { (arg0: string): void; new(): any; }; }; }) => {
   try {
     let newDocument = {
       name: req.body.name,
@@ -46,7 +47,7 @@ router.post("/", async (req, res) => {
 });
 
 // This section will help you update a record by id.
-router.patch("/:id", async (req, res) => {
+router.patch("/:id", async (req: { params: { id: number; }; body: { name: any; position: any; level: any; }; }, res: { send: (arg0: UpdateResult<Document>) => { (): any; new(): any; status: { (arg0: number): void; new(): any; }; }; status: (arg0: number) => { (): any; new(): any; send: { (arg0: string): void; new(): any; }; }; }) => {
   try {
     const query = { _id: new ObjectId(req.params.id) };
     const updates = {
@@ -67,7 +68,7 @@ router.patch("/:id", async (req, res) => {
 });
 
 // This section will help you delete a record
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", async (req: { params: { id: number; }; }, res: { send: (arg0: DeleteResult) => { (): any; new(): any; status: { (arg0: number): void; new(): any; }; }; status: (arg0: number) => { (): any; new(): any; send: { (arg0: string): void; new(): any; }; }; }) => {
   try {
     const query = { _id: new ObjectId(req.params.id) };
 
@@ -80,5 +81,5 @@ router.delete("/:id", async (req, res) => {
     res.status(500).send("Error deleting record");
   }
 });
-
+*/
 export default router;
